8dd5d3a36fc7fb67f55880646ac7104d449f6cbe
Rick <dirklo@gmail.com>
2021-02-23 12:07:28 +0100

    commit tree 9d69b64d842256250eee9e68a271580ac20d8fae

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,29 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update ]
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    new_post = Post.new(post_params)
+    new_post.id = params[:id]
+    if new_post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      @post = new_post
+      render 'posts/edit'
+    end 
   end
 
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
   def post_params
     params.permit(:title, :category, :content)
   end
